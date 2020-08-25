466dde112096ff678d26c162a4ec6e9176bcef47
nickuzmenkov <nickuzmenkov@yahoo.com>
2020-08-25 22:50:38 +0300

    Major 0.0.7 Update

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ requirements = ["ipython>=6", "nbformat>=4", "nbconvert>=5", "requests>=2"]
 
 setup(
     name="pyjou",
-    version="v0.3-alpha",
+    version="v0.0.7",
     author="Nick Kuzmenkov",
     author_email="nickuzmenkov@yahoo.com",
     description="Fluent Journal API",
